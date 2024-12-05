a85ba2c8990abdd0642ca99eb496b85d21aabe3d
Cumcordia <maksimmakarov228666@gmail.com>
2024-12-05 23:48:01 +0500

    init2

--- src/main/java/kz/goldenfish/goldenfish/repository/BookingLogRepository.java
+++
@@ -1,8 +0,0 @@
-package kz.goldenfish.goldenfish.repository;
-import kz.goldenfish.goldenfish.model.BookingLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingLogRepository extends JpaRepository<BookingLog, Long> {
-}
